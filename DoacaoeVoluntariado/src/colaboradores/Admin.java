b8d2b97636b050e61c1357617231ccfe90d7e9d3
hemanuela-fernandes <fernandeshemanuela@gmail.com>
2025-11-24 20:36:40 -0400

    add de login admin

---
+++ DoacaoeVoluntariado/src/colaboradores/Admin.java
@@ -0,0 +1,29 @@
+package colaboradores;
+
+public class Admin {
+	private String nome;
+    private String codigoAcesso ; // senha/código String
+
+    public Admin(String nome) {
+        this.nome = nome;
+        this.codigoAcesso = "a2411min";
+    }
+
+    
+    public boolean autenticar(String codigo) throws Exception {
+        try {
+            if (codigo == null || codigo.isEmpty()) {
+                throw new IllegalArgumentException("Código não pode ser vazio.");
+            }
+
+            return this.codigoAcesso.equals(codigo);
+
+        } catch (Exception e) {
+            throw new Exception("Erro na autenticação: " + e.getMessage());
+        }
+    }
+
+    public String getNome() { return nome; }
+	
+
+}
